53a190cb3623fae35ba07ecef1016b01576988d8
hanzn-zzx <128143343+hanzn-zzx@users.noreply.github.com>
2023-03-29 21:37:02 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
           { text: '六、更多', link: '/deepin-src-col/sec-6' }
         ]
       },
+      {text:'浅谈Deepin Live和修复Linux引导',link:'/deepin-live'}
     ],
     outline: [2, 4], 
     socialLinks: [
