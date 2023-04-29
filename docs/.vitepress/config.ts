6b8c0d977c2932abb37df72d6ca837e9fc5dc58f
pzm9012 <pzm9012@outlook.com>
2023-04-29 21:07:11 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "pzm9012 的文稿(Vitepress)",
-  description: "pzm9012' 的文稿(Vitepress)",
+  description: "pzm9012 的文稿(Vitepress)",
   base: "/doc-vite/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
